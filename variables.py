f107ab170e8205864b19d2835c61b02d4bd6d3cc
pedro fernandez <peferna02@gmail.com>
2023-10-05 23:38:45 -0500

    Primero

---
+++ variables.py
@@ -0,0 +1,16 @@
+#Definir una variable de cada tipo primitivo
+
+nombre = ("pedro fernandez")
+edad = 22
+trabajo = True
+estatura = 1.71
+
+#Resultado = nombre, "pedro fernandez", edad + (22), trabajo, "True", estatura (1.70)
+
+
+
+#Convertir variables a cadenas y concatenarlas
+resultado = nombre + " " + str(22) + " " + str (trabajo) + " " + str(estatura)
+
+#imprimir resultado
+print (resultado)
